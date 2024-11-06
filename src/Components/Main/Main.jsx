c84e573506248915af582b0c6a34c2d08f8e2201
mariodss <mario.soto1400@outlook.com>
2024-11-06 16:31:55 -0500

    first commit

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import './Main.css'
+
+const Main = () => {
+
+    return (
+
+        <div className='main'>
+            <div className="main-left">
+                <h2>Programming courses for everyone</h2>
+                <div>
+                    <p>C++, Python, Java</p>
+                    <p>for everyone</p>
+
+                </div>
+
+                <div className='main-latest-btn'>
+                    <div>Best rated courses</div>
+                    
+                    
+                    
+                    
+                    
+
+                </div>
+
+            </div>
+
+            <div className="main-right">
+               
+
+            </div>
+
+
+
+        </div>
+
+
+
+    )
+
+
+}
+
+export default Main
