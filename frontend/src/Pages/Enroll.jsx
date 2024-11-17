33f34daa1e62dea781fe2e9dac1de666715d15c3
mariodss <mario.soto1400@outlook.com>
2024-11-17 16:07:37 -0500

    Initial HomePage Design

--- frontend/src/Pages/Enroll.jsx
+++ frontend/src/Pages/Enroll.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Hero from '../Components/Hero/Hero'
 
 
 
@@ -9,7 +10,7 @@ const Enroll = () => {
         
         <div>
 
-
+            <Hero/>
 
 
         </div>
